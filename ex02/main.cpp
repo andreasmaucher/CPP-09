d235fff4532134ba21e1268286ae9428f63d16e9
amaucher <andreas-maucher@gmx.de>
2025-09-04 19:58:48 +0200

    first iteration of Jacobsthal sequence

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -4,7 +4,7 @@ int main(int ac, char **av) {
     if (ac < 2) 
     {
         std::cerr << "Error: No arguments provided" << std::endl;
-        std::cerr << "Usage: ./PmergeMe <positive_integer1> <positive_integer2> ..." << std::endl;
+        std::cerr << "Usage: ./PmergeMe <positive_integer1> <positive_integer2> etc. ..." << std::endl;
         return 1;
     }
     try {
